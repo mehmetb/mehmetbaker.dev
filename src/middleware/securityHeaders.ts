deb03b1f0758ffbdb86b5d4c2765e01deb609509
Mehmet Baker <mehmet@mehmetbaker.dev>
2022-03-13 19:33:18 +0300

    Updated Content-Security-Policy, Feature-Policy and Permissions-Policy
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- src/middleware/securityHeaders.ts
+++ src/middleware/securityHeaders.ts
@@ -1,4 +1,57 @@
-import { Context } from "sunder";
+import { Context } from 'sunder';
+
+const FEATURES: string[] = [
+  'accelerometer',
+  'ambient-light-sensor',
+  'autoplay',
+  'battery',
+  'camera',
+  'display-capture',
+  'document-domain',
+  'encrypted-media',
+  'execution-while-not-rendered',
+  'execution-while-out-of-viewport',
+  'fullscreen',
+  'gamepad',
+  'geolocation',
+  'gyroscope',
+  'layout-animations',
+  'legacy-image-formats',
+  'magnetometer',
+  'microphone',
+  'midi',
+  'navigation-override',
+  'oversized-images',
+  'payment',
+  'picture-in-picture',
+  'publickey-credentials-get',
+  'speaker-selection',
+  'sync-xhr',
+  'unoptimized-images ',
+  'unsized-media ',
+  'usb',
+  'screen-wake-lock',
+  'web-share',
+  'xr-spatial-tracking'
+];
+
+const FEATURE_POLICY: string = FEATURES
+  .map(feature => `${feature} 'none'`)
+  .join('; ');
+
+const PERMISSIONS_POLICY: string = FEATURES
+  .map(feature => `${feature}=()`)
+  .join(', ');
+
+const CONTENT_SECURITY_POLICY: string = [
+  `default-src 'self'`,
+  `style-src  'self' https://fonts.googleapis.com 'unsafe-inline'`,
+  `script-src 'self' https://cdn.jsdelivr.net`,
+  `font-src 'self' https://fonts.gstatic.com`,
+  `frame-ancestors 'none'`,
+  `object-src 'none'`,
+  `base-uri 'self'`,
+].join('; ');
 
 /**
  * Example middleware that adds a custom header
@@ -8,6 +61,8 @@ export async function securityHeaders(ctx: Context, next: Function) {
   ctx.response.headers.set('X-Content-Type-Options', 'nosniff');
   ctx.response.headers.set('X-Frame-Options', 'DENY');
   ctx.response.headers.set('Referrer-Policy', 'same-origin');
-  ctx.response.headers.set('Feature-Policy', 'none');
+  ctx.response.headers.set('Feature-Policy', FEATURE_POLICY);
+  ctx.response.headers.set('Permissions-Policy', PERMISSIONS_POLICY);
+  ctx.response.headers.set('Content-Security-Policy', CONTENT_SECURITY_POLICY);
   await next();
 }
