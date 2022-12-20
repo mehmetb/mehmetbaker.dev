8335186be5d3e0975a30110085fdefd4b599fc9c
Mehmet Baker <mehmet@mehmetbaker.dev>
2022-12-20 23:22:40 +0300

    Removed src/ dir
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "./app";
-
-const app = createApp();
-
-export default {
-  async fetch(request: Request, env: any, ctx: any) {
-    return app.fetch(request, env, ctx);
-  }
-}
--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "sunder";
-import { Env } from "./bindings";
-import { serveStaticAssetsFromKV, serveIndex } from "./middleware/static";
-
-export function registerRoutes(router: Router<Env>) {
-  router.get("/", serveIndex);
-  router.get("/:assetPath+", serveStaticAssetsFromKV);
-  router.head("/:assetPath+", serveStaticAssetsFromKV);
-}
